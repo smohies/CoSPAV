23e040e9dcf1cf15c9bee3da6f9508e9f9db4c46
Samuel Odriozola <samuel.martinez2@epicor.com>
2022-11-11 18:24:39 -0600

    Regex ping stdout

--- CoSPAV.py
+++ CoSPAV.py
@@ -1,4 +1,5 @@
 import os
+import re
 import csv
 import subprocess
 import platform
@@ -12,14 +13,17 @@ def validate_file(filename, description):
             break
     return filename
 
+# Get ping times in config file?
 def ping(host):
     param = "-n" if platform.system().lower() == "windows" else "-c"
-    command = ["ping", param, "1", host]
-    ping_result = subprocess.run(command)
+    ping_times = "1"
+    command = ["ping", param, ping_times, host]
+    ping_result = subprocess.run(command, capture_output=True)
     if ping_result.returncode == 0:
-        return "Good Ping!"
+        result = re.search(r".*\((\d*% loss)\).*Average = (\d*ms).*", str(ping_result.stdout))
+        return f"{result.group(1)} @ {result.group(2)}"
     else:
-        return "Bad Ping!"
+        return "FAIL"
 
 def main():
 
@@ -27,6 +31,10 @@ def main():
     rvtools_csv = validate_file("2.csv", "RVTools")
   
     print(f"Files {epn_iapise_inv_csv} and {rvtools_csv} found!")
+    # Test
+    print(ping("www.google.com"))
+    print(ping("www.amazon.com"))
+    print(ping("www.wawaxuxu.mex"))
     
 if __name__ == "__main__":
     main()
