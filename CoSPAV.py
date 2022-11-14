84c80cdad337cdce12418de1d7927a5946aa29e5
Samuel Odriozola <samuel.martinez2@epicor.com>
2022-11-14 14:34:09 -0600

    Open csv and create server_ip dic

--- CoSPAV.py
+++ CoSPAV.py
@@ -15,6 +15,7 @@ def validate_file(filename, description):
 
 def ping(host):
     param = "-n" if platform.system().lower() == "windows" else "-c"
+    # will point to config
     ping_times = "1"
     command = ["ping", param, ping_times, host]
     ping_result = subprocess.run(command, capture_output=True)
@@ -26,14 +27,30 @@ def ping(host):
 
 def main():
 
-    inv_csv = validate_file("inv.csv", "EPN IAPISE Inventory")
-    rvtools_csv = validate_file("rvtools.csv", "RVTools")
+    inv_csv_filename = validate_file("inv.csv", "EPN IAPISE Inventory")
+    rvtools_csv_filename = validate_file("tools.csv", "RVTools")
   
-    print(f"Files {inv_csv} and {rvtools_csv} found!")
-    # Test
-    print(ping("www.google.com"))
-    print(ping("www.amazon.com"))
-    print(ping("www.wawaxuxu.mex"))
+    print(f"Files {inv_csv_filename} and {rvtools_csv_filename} found!")
+    print(f"Loading data from {inv_csv_filename}")
+    
+    servers_ip = {}
+    # will point to config
+    inv_csv_header = 1
+    inv_csv_ip_col = 0
+    inv_csv_name_col = 1
+    inv_csv_hostname_col = 2
+    inv_csv_os_col = 3
+    
+    with open(inv_csv_filename, "r") as inv_csv_file:
+        inv_csv = csv.reader(inv_csv_file)
+        line_count = 0
+        while line_count <= inv_csv_header:
+            next(inv_csv)
+            line_count += 1
+        for row in inv_csv:
+            servers_ip[row[inv_csv_ip_col].replace("Â", "").strip()] = [row[inv_csv_name_col].strip(), "", False, row[inv_csv_hostname_col].strip(), "", False, row[inv_csv_os_col].strip(), "", False]
+        for key in servers_ip:
+            print(f"{key} : {servers_ip[key]}")
     
 if __name__ == "__main__":
     main()
